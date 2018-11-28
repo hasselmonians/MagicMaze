b0ffcb023c34d83bc02674a84c22ec473b522654
= <alec.hoyland@gmail.com>
2018-11-28 14:36:27 -0500

    Add a serial callback function to run whenever there are bytes available

---
+++ @MagicMaze/serialCallback.m
@@ -0,0 +1,8 @@
+function serialCallback(self)
+
+  % callback function to run whenever there are at least four bytes in the buffer
+  % calls serialInterpret and displays to the GUI
+
+  self.exp_state = self.serialInterpret(fscanf(self.serial_port, '%d'));
+
+end % function
