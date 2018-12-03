dd90e434edb6890a51e05fe6558fb25587b58295
= <alec.hoyland@gmail.com>
2018-12-03 12:35:26 -0500

    Perform the experiment

---
+++ @MagicMaze/doExperiment.m
@@ -0,0 +1,36 @@
+function doExperiment(self)
+
+  % performs the protocol specified in self.protocol
+  % records the data in self.outcomes based on the conditions set in self.trials
+  % updates the experimental state in self.exp_state
+
+  self.outcomes = NaN(length(self.trials), 1);
+
+  for ii = 1:length(self.trials)
+
+    % send trial information through the serial port
+    fprintf(self.serial_port, num2str(uint8(self.trials(ii))));
+
+    % wait for a response
+    waitfor(self, 'isDone', logical(1));
+
+    switch self.protocol
+    case 'dmns'
+      if self.exp_state == 3
+        self.outcomes(ii) = 1;
+      else
+        self.outcomes(ii) = 0;
+      end
+    otherwise
+      disp('[ERROR] unknown protocol')
+    end
+
+  end % ii
+
+  disp('[INFO] experiment completed')
+
+  % sing the song of our people
+  load handel
+  sound(y,Fs)
+
+end % function
